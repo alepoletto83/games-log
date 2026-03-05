a3ae0267fae91ad705c92e00dd07debb5413169b
Alexandre Poletto <apoletto83@gmail.com>
2026-03-04 20:22:56 -0800

    adicionado local storage

--- src/hooks/useBacklogStore.tsx
+++ src/hooks/useBacklogStore.tsx
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import type { Game } from '../api/gameService';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 interface BacklogState {
   gameBacklog: Game[];
@@ -7,17 +8,25 @@ interface BacklogState {
   removeGame: (gameId: number) => void;
 }
 
-export const useBacklogStore = create<BacklogState>((set) => ({
-  gameBacklog: [],
+export const useBacklogStore = create<BacklogState>()(
+  persist(
+    (set) => ({
+      gameBacklog: [],
 
-  addGame: (game) =>
-    set((state) => {
-      if (state.gameBacklog.some((g) => g.id === game.id)) return state;
-      return { gameBacklog: [...state.gameBacklog, game] };
+      addGame: (game) =>
+        set((state) => {
+          if (state.gameBacklog.some((g) => g.id === game.id)) return state;
+          return { gameBacklog: [...state.gameBacklog, game] };
+        }),
+
+      removeGame: (gameId) =>
+        set((state) => ({
+          gameBacklog: state.gameBacklog.filter((g) => g.id !== gameId),
+        })),
     }),
-
-  removeGame: (gameId) =>
-    set((state) => ({
-      gameBacklog: state.gameBacklog.filter((g) => g.id !== gameId),
-    })),
-}));
+    {
+      name: 'games-log-storage',
+      storage: createJSONStorage(() => localStorage),
+    },
+  ),
+);
